75f3d5b135e899675ad4f32e4391ecb9d18ae02a
ivansukhoparov <rbcdaaa@gmail.com>
2023-12-08 21:50:31 +0100

    add sorting for users

--- src/repositories/users-query-repository.ts
+++ src/repositories/users-query-repository.ts
@@ -1,30 +1,27 @@
 import {SearchUsersRepositoryType, SortUsersRepositoryType} from "../types/users/input";
-import {blogCollection, usersCollection} from "../db/db-collections";
+import {usersCollection} from "../db/db-collections";
 import {WithId} from "mongodb";
-import {BlogType} from "../types/blogs/output";
-import {blogMapper} from "../types/blogs/mapper";
-import {usersRouter} from "../routers/users-router";
 import {UserType} from "../types/users/output";
 import {userMapper} from "../types/users/mapper";
 
 
-export class UsersQueryRepository{
+export class UsersQueryRepository {
 
-    static async getAllUsers(sortData:SortUsersRepositoryType, searchData:SearchUsersRepositoryType){
-        let searchKeysArray=[];
-        let sortKey, searchKey = {};
+    static async getAllUsers(sortData: SortUsersRepositoryType, searchData: SearchUsersRepositoryType) {
+        let searchKeysArray = [];
+        let sortKey = {};
+        let searchKey = {};
         let sortDirection: number;
 
-        // check if have searchNameTerm create search key
+        // check have searchNameTerm create search key
         if (searchData.searchLoginTerm) searchKeysArray.push({login: {$regex: searchData.searchLoginTerm, $options: "i"}});
         if (searchData.searchEmailTerm) searchKeysArray.push({email: {$regex: searchData.searchEmailTerm, $options: "i"}});
 
-        if (searchKeysArray.length===1){
-            searchKey=searchKeysArray[0];
-        }else if (searchKeysArray.length>1){
-            searchKey={$or: searchKeysArray};
+        if (searchKeysArray.length === 1) {
+            searchKey = searchKeysArray[0];
+        } else if (searchKeysArray.length > 1) {
+            searchKey = {$or: searchKeysArray};
         }
-
         // calculate limits for DB request
         const documentsTotalCount = await usersCollection.countDocuments(searchKey); // Receive total count of blogs
         const pageCount = Math.ceil(documentsTotalCount / +sortData.pageSize); // Calculate total pages count according to page size
@@ -34,11 +31,9 @@ export class UsersQueryRepository{
         if (sortData.sortDirection === "desc") sortDirection = -1;
         else sortDirection = 1;
 
-        // check if have fields exists assign the same one else assign "createdAt" value
-        if (sortData.sortBy === "description") sortKey = {description: sortDirection};
-        else if (sortData.sortBy === "websiteUrl") sortKey = {websiteUrl: sortDirection};
-        else if (sortData.sortBy === "name") sortKey = {name: sortDirection};
-        else if (sortData.sortBy === "isMembership") sortKey = {isMembership: sortDirection};
+        // check have fields exists assign the same one else assign "createdAt" value
+        if (sortData.sortBy === "login") sortKey = {login: sortDirection};
+        else if (sortData.sortBy === "email") sortKey = {email: sortDirection};
         else sortKey = {createdAt: sortDirection};
 
         // Get documents from DB
@@ -51,6 +46,5 @@ export class UsersQueryRepository{
             totalCount: documentsTotalCount,
             items: users.map(userMapper)
         }
-
     }
 }
