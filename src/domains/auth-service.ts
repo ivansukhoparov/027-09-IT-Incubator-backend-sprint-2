1c6e7729be68d755a060de58c1b4362d4bce2dd3
ivansukhoparov <rbcdaaa@gmail.com>
2023-12-15 23:32:56 +0100

    add auth service getUserIdByToken

--- src/domains/auth-service.ts
+++ src/domains/auth-service.ts
@@ -4,9 +4,10 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import {AuthOutputType} from "../types/auth/otput";
+import {runDB} from "../db/db";
 
 dotenv.config();
-const salt = process.env.SECRET_KEY!
+const secretKey = process.env.SECRET_KEY!
 export class AuthService {
     static async authUser(loginOrEmail: string, password: string): Promise<AuthOutputType | null> {
         const user:UserAuthOutputType|null = await UsersRepository.getUserByLoginOrEmail(loginOrEmail);
@@ -15,9 +16,17 @@ export class AuthService {
         const isSuccess = await bcrypt.compare(password, user.hash);
         if (!isSuccess) return null;
 
-        const token = jwt.sign({userId: user.id}, salt, {expiresIn: "1h"});
+        const token = jwt.sign({userId: user.id}, secretKey, {expiresIn: "1h"});
 
         return {accessToken: token}
     }
 
+    static async getUserIdByToken(token:string):Promise<string|null>{
+        try {
+            const result:any = jwt.verify(token, secretKey);
+            return result.userId
+        }catch (err){
+            return null
+        }
+    }
 }
