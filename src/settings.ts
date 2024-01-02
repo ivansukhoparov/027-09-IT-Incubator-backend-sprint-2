d90045448c39f6827b3d537b80a5d5635f6d4c77
ivansukhoparov <rbcdaaa@gmail.com>
2024-01-02 23:18:48 +0100

    add cookie parser and example

--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, {Request, Response} from "express";
 import {videosRouter} from "./routers/videos-router";
 import {testingRouter} from "./routers/testing-router";
 import {blogsRouter} from "./routers/blogs-router";
@@ -6,10 +6,12 @@ import {postsRouter} from "./routers/posts-router";
 import {authRouter} from "./routers/auth-router";
 import {usersRouter} from "./routers/users-router";
 import {commentsRouter} from "./routers/comments-router";
+import cookieParser from "cookie-parser";
 
 export const app = express();
 
 app.use(express.json());
+app.use(cookieParser())
 
 app.use("/testing", testingRouter);
 
@@ -19,3 +21,13 @@ app.use("/posts", postsRouter);
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
 app.use("/comments", commentsRouter);
+
+
+app.post('/auth/example', (req: Request, res: Response) => {
+    res.cookie('cookie_name', "htis is cookei! HA!", {httpOnly: true,secure: true})
+    res.status(204).send('Hello samurai from it-incubator!!!')
+})
+app.get('/auth/result', async (req: Request, res: Response) => {
+    const cookie_name= req.cookies
+    res.status(200).json(cookie_name)
+})
